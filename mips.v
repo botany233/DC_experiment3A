d9bc3324d8a15f90b4961fa9ac07ba2a91604438
botany233 <18201168383@163.com>
2024-11-09 11:17:57 +0800

    数据选择器整理

--- mips.v
+++ mips.v
@@ -38,24 +38,6 @@ module mips(
     //指令跳转地址
     assign pc_jump = {pc_plus_4[31:28], instr[25:0], 2'b0};
 
-    //选择0:寄存器写入内容中间量2还是1:指令地址+4作为寄存器写入内容
-    mux2 #(32) mux2_10(.a(result_extimm), .b(pc_plus_4), .s(pc_to_reg), .y(reg_wrd));
-
-    //选择0:寄存器写入内容中间量1还是1:扩展立即数作为寄存器写入内容中间量2
-    mux2 #(32) mux2_8(.a(result), .b(sign_imm), .s(lui), .y(result_extimm));
-
-    //选择0:指令地址+4还是1:指令地址转移分支作为下一条指令地址中间量1
-    mux2 #(32) mux2_7(.a(pc_plus_4), .b(pc_branch), .s(branch), .y(pc_sel_1));
-
-    //选择0:alu运算结果还是1:存储器输出作为alu&存储器输出
-    mux2 #(32) mux2_6(.a(alu_result), .b(read_data), .s(mem_to_reg), .y(result));
-
-    //选择0:下一条指令地址中间量1还是1:指令跳转地址作为下一条指令地址中间量2
-    mux2 #(32) mux2_3(.a(pc_sel_1), .b(pc_jump), .s(jump), .y(pc_sel_2));
-
-    //选择0:下一条指令地址中间量2还是1:寄存器输出1作为下一条指令地址
-    mux2 #(32) mux2_5(.a(pc_sel_2), .b(r1), .s(jr), .y(pc_next));
-
     //指令译码
     controller controller1(
         .op_i(instr[31:26]),
@@ -91,22 +73,10 @@ module mips(
         .r2(r2)
     );
 
-    //选择0:寄存器输出1还是1:寄存器输出2作为alu输入1
-    mux2 #(32) mux2_4(.a(r1), .b(sign_imm), .s(alu_srca), .y(srca));
-
-    //选择0:rt还是1:rd作为寄存器写入地址中间量
-    mux2 #(5) mux2_1(.a(instr[20:16]), .b(instr[15:11]), .s(reg_dst), .y(rt_rd));
-
-    //选择0:寄存器写入地址中间量还是1:31($ra)作为寄存器写入地址
-    mux2 #(5) mux2_9(.a(rt_rd), .b(5'b11111), .s(ra_dst), .y(reg_wra));
-
     //立即数有符号扩展
     sign_extend sign_extend1(.imm(instr[15:0]), .sign_imm(sign_imm), .ext_op(ext_op));
 
-    //选择0:寄存器输出2还是1:扩展后立即数作为alu输入2
-    mux2 #(32) mux2_2(.a(r2), .b(sign_imm), .s(alu_srcb), .y(srcb));
-
-    //alu
+    //ALU
     alu alu1(
         .srca_i(srca),
         .srcb_i(srcb),
@@ -114,4 +84,34 @@ module mips(
         .zero(zero),
         .alu_result(alu_result)
     );
+
+    //选择0:rt还是1:rd作为寄存器写入地址中间量
+    mux2 #(5) mux2_1(.a(instr[20:16]), .b(instr[15:11]), .s(reg_dst), .y(rt_rd));
+
+    //选择0:寄存器输出2还是1:扩展后立即数作为alu输入2
+    mux2 #(32) mux2_2(.a(r2), .b(sign_imm), .s(alu_srcb), .y(srcb));
+
+    //选择0:下一条指令地址中间量1还是1:指令跳转地址作为下一条指令地址中间量2
+    mux2 #(32) mux2_3(.a(pc_sel_1), .b(pc_jump), .s(jump), .y(pc_sel_2));
+
+    //选择0:寄存器输出1还是1:寄存器输出2作为alu输入1
+    mux2 #(32) mux2_4(.a(r1), .b(sign_imm), .s(alu_srca), .y(srca));
+
+    //选择0:下一条指令地址中间量2还是1:寄存器输出1作为下一条指令地址
+    mux2 #(32) mux2_5(.a(pc_sel_2), .b(r1), .s(jr), .y(pc_next));
+
+    //选择0:alu运算结果还是1:存储器输出作为alu&存储器输出
+    mux2 #(32) mux2_6(.a(alu_result), .b(read_data), .s(mem_to_reg), .y(result));
+
+    //选择0:指令地址+4还是1:指令地址转移分支作为下一条指令地址中间量1
+    mux2 #(32) mux2_7(.a(pc_plus_4), .b(pc_branch), .s(branch), .y(pc_sel_1));
+
+    //选择0:寄存器写入内容中间量1还是1:扩展立即数作为寄存器写入内容中间量2
+    mux2 #(32) mux2_8(.a(result), .b(sign_imm), .s(lui), .y(result_extimm));
+
+    //选择0:寄存器写入地址中间量还是1:31($ra)作为寄存器写入地址
+    mux2 #(5) mux2_9(.a(rt_rd), .b(5'b11111), .s(ra_dst), .y(reg_wra));
+
+    //选择0:寄存器写入内容中间量2还是1:指令地址+4作为寄存器写入内容
+    mux2 #(32) mux2_10(.a(result_extimm), .b(pc_plus_4), .s(pc_to_reg), .y(reg_wrd));
 endmodule
