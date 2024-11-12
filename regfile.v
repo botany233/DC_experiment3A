0c47c0e520c2f8b176e71f2f343ae36c81ca2f61
botany233 <18201168383@163.com>
2024-11-12 13:33:55 +0800

    debug

--- regfile.v
+++ regfile.v
@@ -1,6 +1,6 @@
 //寄存器组
 module regfile(
-    input clk, we,//始终，写使能
+    input clk, we,//时钟，写使能
     input [4:0] a1, a2, a3,//读地址1，2，写地址3
     input [31:0] wd,//写数据
     output [31:0] r1, r2//读数据1，2
